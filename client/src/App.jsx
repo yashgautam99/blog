ba595738dd6aa0eecfa481b8dd7ca84b30bfb699
Yash Gautam <yash99gautam@gmail.com>
2024-09-15 18:24:13 +0530

    created navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import About from "./pages/About";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
